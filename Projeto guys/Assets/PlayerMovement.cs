fb599f3eb1a241c17f06bbf6c23d9528c2a45082
Neko Neko <142621928+NekoNekoDev@users.noreply.github.com>
2026-09-17 22:24:06 -0300

    criando o player e o script de movimentacao

---
+++ Projeto guys/Assets/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float moveSpeed = 5f;
+
+    float horizontalMovement;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 newVelocity = new Vector2(horizontalMovement * moveSpeed, rb.linearVelocity.y);
+        rb.linearVelocity = newVelocity;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontalMovement = context.ReadValue<Vector2>().x;
+    }
+}
